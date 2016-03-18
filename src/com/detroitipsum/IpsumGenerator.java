31601aa5ec0bb2f611211a5a28e059834462a584
Jeseekia Vaughn <jeseekia@gmail.com>
2016-03-18 07:10:42 -0400

    Planning out an idea for logic

--- src/com/detroitipsum/IpsumGenerator.java
+++ src/com/detroitipsum/IpsumGenerator.java
@@ -14,28 +14,52 @@ package com.detroitipsum;
  */
 public class IpsumGenerator {
 	
-	private String[] words = {"Detroit","coney dog", "coney island", "People Mover", "Pistons", 
+	private String[] detroitWords = {"Detroit","coney dog", "coney island", "People Mover", "Pistons", 
 								"Tigers", "Lions", "rebirth", "motor city","Greektown","Downtown",
 									"Wayne State University", "Corktown","Midtown","New Center","Mexican Town",
 										"Ford", "GM", "Chrysler", "like a rock", "the big three",
 											"Quicken Loans", "Ilitch family"};
 	
-	public String getWords(int numOfWords) {
-			
-		return "";
+	public String[] getWords(int numOfWords) {
+		String[] words = {""};
+		//For numOfWords
+			//get a random word
+		
+		//Return the word(s)
+		return words;
 	}
 
-	public String buildSentence() {
+	public String buildSentence(int numOfSentences) {
 		
+		//Determine a sentence length between 7-11 words
+		
+		//For numOfSentences
+			//Create a sentence string with a numOfWords
+			//Capitalize the first letter of the sentence
+			//Add a period to the end of the sentence
+		
+		//Return the sentence(s)
 		return "";
 	}
 	
-	public String buildParagraph() {
+	public String buildParagraph(int numOfParagraphs) {
+		
+		//Determine the paragraph size based on numOfParagraphs (small,medium,large)
+		
+		//For a numOfParagraphs
+			//Build a paragraph with a certain number of sentences
+		
+		//Return the paragraph(s)
 			
 		return "";
 	}
 	
-	public String ipsumParagraph() {
+	public String ipsumParagraph(int paragraphSize, int numOfParagraphs) {
+		
+		//For a numOfParagraphs
+			//Build some ipsum
+		
+		//Return the ipsum
 		
 		return "";
 	}
