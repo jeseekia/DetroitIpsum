f330e3b0938daa6abfd77ae98cdfc61e03c9c666
Jeseekia Vaughn <jeseekia@gmail.com>
2016-03-23 18:26:06 -0400

    Adding a directory for the test file(s) and adding some starter test templates to be modified later.

---
+++ tests/com/detroitipsum/IpsumGeneratorTest.java
@@ -0,0 +1,31 @@
+package com.detroitipsum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jeseekia on 3/23/16.
+ */
+public class IpsumGeneratorTest {
+
+    @Test
+    public void testGetWords() throws Exception {
+
+    }
+
+    @Test
+    public void testBuildSentence() throws Exception {
+
+    }
+
+    @Test
+    public void testBuildParagraph() throws Exception {
+
+    }
+
+    @Test
+    public void testIpsumParagraph() throws Exception {
+
+    }
+}
